fd95a98dfddad766d7f51f93f9308c79249fd7e0
gustavo-sobbrero-develcode <gustavo.sobbrero@develcode.com.br>
2022-08-11 13:57:15 -0300

    criar token do usuário

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,53 @@
+import { inject, injectable } from "tsyringe";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+    if (!user) {
+      throw new Error("E-mail or password incorrect!");
+    }
+
+    const passwordMatched = await compare(password, user.password);
+    if (!passwordMatched) {
+      throw new Error("E-mail or password incorrect!");
+    }
+
+    const token = sign({}, "240fee48cfdd0ec9ac6b6876bdef1d4f", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+    };
+
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserUseCase };
