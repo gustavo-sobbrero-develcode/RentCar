84b22a09011d2cdf553a43a57703d1c2963b7fee
gustavo-sobbrero-develcode <gustavo.sobbrero@develcode.com.br>
2022-10-14 15:13:39 -0300

    middleware for admin permissions

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+import { ensureAdmin } from "../middlewares/ensureAdmin";
 
 const carsRoutes = Router();
 
 let createCarController = new CreateCarController();
 
-carsRoutes.post("/", createCarController.handle);
+carsRoutes.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createCarController.handle
+);
 
 export { carsRoutes };
